5ce7c7f6bace32144db26ea94c6a1868d35506e8
kashima19960 <2494389920@qq.com>
2025-04-22 10:44:55 +0800

    取消注释npm安装命令以确保依赖项正确安装

--- init.py
+++ init.py
@@ -1,4 +1,4 @@
 import os 
-# os.system(f"npm install hexo-renderer-pug hexo-renderer-stylus --save&&npm install&&npm install hexo-deployer-git --save&&npm install hexo-generator-search --save&&npm install hexo-wordcount --save")
+os.system(f"npm install hexo-renderer-pug hexo-renderer-stylus --save&&npm install&&npm install hexo-deployer-git --save&&npm install hexo-generator-search --save&&npm install hexo-wordcount --save")
 
 os.system(f"hexo clean&&hexo generate&&hexo server")
